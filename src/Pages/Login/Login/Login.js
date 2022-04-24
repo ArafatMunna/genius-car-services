c0c34927280701245c832eec6d4eda761613a841
ArafatMunna <arafatmunna000@gmail.com>
2022-04-25 02:46:21 +0600

    jwt added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -10,6 +10,7 @@ import SocialLogin from "../SocialLogin/SocialLogin";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PageTitle from "../../Shared/PageTitle/PageTitle";
+import axios from "axios";
 
 const Login = () => {
     const emailRef = useRef("");
@@ -29,7 +30,7 @@ const Login = () => {
 
     useEffect(() => {
         if (user) {
-            navigate(from, { replace: true });
+            // navigate(from, { replace: true });
         }
     }, [user]);
 
@@ -37,13 +38,19 @@ const Login = () => {
         errorElement = <p className="text-danger">Error: {error?.message}</p>;
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+
+        const { data } = await axios.post("http://localhost:5000/login", {
+            email,
+        });
+        localStorage.setItem("accessToken", data.accessToken);
+        navigate(from, { replace: true });
     };
 
     const resetPassword = async () => {
--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,14 +1,33 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import axiosPrivate from "../../api/axiosPrivate";
 
 const Order = () => {
+    const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const getOrders = async () => {
-            const url = `http://localhost:5000/order`;
-            const { data } = await axios.get(url);
-            setOrders(data);
+            const email = user?.email;
+
+            const url = `http://localhost:5000/order?email=${email}`;
+            try {
+                const { data } = await axiosPrivate.get(url);
+                setOrders(data);
+            } catch (error) {
+                console.log(error.message);
+                if (
+                    error.response.status === 401 ||
+                    error.response.status === 403
+                ) {
+                    signOut(auth);
+                    navigate("/login");
+                }
+            }
         };
         getOrders();
 
@@ -16,7 +35,7 @@ const Order = () => {
             .get("http://localhost:5000/order")
             .then((res) => setOrders(res.data))
             .catch((err) => console.log(err)); */
-    }, []);
+    }, [user]);
     return (
         <div>
             <h2>Your Orders: {orders?.length}</h2>
